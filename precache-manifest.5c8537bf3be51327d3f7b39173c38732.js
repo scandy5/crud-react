1361fc47e7731f2e0cdce146ffdef6666eae5ab5
Sergey <scandy_boss@ukr.net>
2019-02-26 20:57:06 +0200

    Updates

---
+++ precache-manifest.5c8537bf3be51327d3f7b39173c38732.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8ae5259a6b519baa6e5",
+    "url": "/crud-react/static/js/runtime~main.a8ae5259.js"
+  },
+  {
+    "revision": "380e408766244fcad658",
+    "url": "/crud-react/static/js/main.380e4087.chunk.js"
+  },
+  {
+    "revision": "8c2760832afca6479b5b",
+    "url": "/crud-react/static/js/2.8c276083.chunk.js"
+  },
+  {
+    "revision": "380e408766244fcad658",
+    "url": "/crud-react/static/css/main.85a8c610.chunk.css"
+  },
+  {
+    "revision": "a35164fd3842c194498f22ea24c53bc1",
+    "url": "/crud-react/index.html"
+  }
+];
